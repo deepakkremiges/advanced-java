f1af629b902c410af786360b0234d17e35211a23
Deepak Kumar <deepak.kumar@remiges.tech>
2024-03-04 15:28:03 +0530

    First two problems

---
+++ src/test/java/com/ndml_assignment/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndml_assignment.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
